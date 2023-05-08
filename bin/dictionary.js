4edb64c42f5d1cd14ee4ceb14fe2f15b08b76ca3
dorian3343 <dorian3343@gmail.com>
2023-05-08 12:09:51 +0200

    dictionary 2

--- bin/dictionary.js
+++ bin/dictionary.js
@@ -1,18 +1,64 @@
-const Map1 = new Map([ 
+
+  const Map1 = new Map([ 
   ["<p>", "p!{"],
   ["<h1>", "h1!{"],
   ["<h2>", "value3"],
   ["<h3>", "value3"],
   ["<h4>", "value3"],
   ["<h5>", "value3"],
-  ["<h6>", "value3"]
+  ["<h6>", "value3"],
+  ["div", "<div>"],
+  ["</div>","</div>"],
+  ["<div style=","<div style="],
+  [">",">"],
+  ["<span style=","<span style="],
+  ["</span>","</span>"],
+  ["<span>","<span>"],
+  ["pt;","pt;"],
+  ["px;","px;"],
+  ["font-size:","font-size:"],
+  [";",";"],
+  ["font-family:","font-family:"],
+  ["justify;","justify;"],
+  ["color:","color:"],
+  ["rgb(","rgb("],
+  [");",");"],
+  ["padding-","padding-"],
+  ["center;","center;"],
+  ["left;","left;"],
+  ["right;","right;"],
+  ["</p>","</p>"],
+  ["background-","background-"],
+  ["line-height","line-height"],
+  ["<p style=","<p style="],
+  ["<h1 style=","<h1 style="],
+  ["<h2 style=","<h2 style="],
+  ["<h3 style=","<h3 style="],
+  ["<h4 style=","<h4 style="],
+  ["<h5 style=","<h5 style="],
+  ["<h6 style=","<h6 style="],
+  ["</h1>","</h1>"],
+  ["</h2>","</h2>"],
+  ["</h3>","</h3>"],
+  ["</h4>","</h4>"],
+  ["</h5>","</h5>"],
+  ["</h6>","</h6>"],
+  ["<pre style=","<pre style="],
+  ["</pre>","</pre>"],
+  ["@","@ma"],
+  ["{","@ot"],
+  ["}","@za"],
+  ["<em>","<em>"],
+  ["</em>","</em>"],
+  ["<strong>","<strong>"],
+  ["</strong>","</strong>"],
 ]);
+
 function stringContainsKey(string, keyMap) {
-  console.log(keyMap)
-  console.log(string)
-  if (keyMap.has(string)){
-console.log("kej")
-  }else{
-    console.log("throw error")
+  console.log(Object.keys)
+  let i = 0
+  while (i < Object.keys(Map1).length){
+    i++
+    console.log(string)
   }
 }
--- bin/displayscript.js
+++ bin/displayscript.js
@@ -4,7 +4,6 @@ tinymce.init({
   document.getElementById('my-form').addEventListener('submit', function (event) {
       event.preventDefault(); // Prevent the default form submission
       var content = tinymce.get('my-editor').getContent(); // Get the content from the editor
-      console.log(content); // Do something with the content, like sending it to the server
       var myDiv = document.getElementById("display");
       myDiv.innerHTML = "<plaintext>"+  content;
       stringContainsKey(content,Map1)
